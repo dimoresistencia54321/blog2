cd66df34b8903d94b5b25fcc5a0a871066fc0f14
damian benitez <damiandimo9490@gmail.com>
2022-09-19 12:23:39 -0300

    cambio base datos

---
+++ ConfigBlog/ConfigBlog/settings/local.py
@@ -0,0 +1,15 @@
+
+from .settings import *
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'blog1',
+        'USER': 'root',
+        'PASSWORD': '635271qwzxAS',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
